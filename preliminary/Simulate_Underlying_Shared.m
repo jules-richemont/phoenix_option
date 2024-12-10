227e0f65c4cafb4ec0805f032652b02607d9f97d
Ambroise THIBAULT <92878091+AmbroiseTHIBAULT@users.noreply.github.com>
2024-12-10 10:42:48 +0100

    new version to simulate underlying with predefined random number

---
+++ preliminary/Simulate_Underlying_Shared.m
@@ -0,0 +1,11 @@
+function [S] = Simulate_Underlying_Shared(S0, r, sigma, T, N, g)
+    % - g: shared random numbers (1 x N)
+    % Output:
+    % - S: vector of simulated prices (1 x (N+1))
+    S = zeros(1, N+1);
+    S(1) = S0;
+    dt = T / N;
+    for i = 1:N
+        S(i+1) = S(i) * exp((r - 0.5 * sigma^2) * dt + sigma * sqrt(dt) * g(i));
+    end
+end
