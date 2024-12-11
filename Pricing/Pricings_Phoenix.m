2de40227acdd763184e701108b52403b198254a6
Ambroise THIBAULT <92878091+AmbroiseTHIBAULT@users.noreply.github.com>
2024-12-11 23:48:06 +0100

    1- we simulate path everytime : we dont have the sames paths for the same MC iteration of the pricing. 2- We give directly the ratio with respect to S_0 to compute the Coupons C_ph & C_Y

---
+++ Pricing/Pricings_Phoenix.m
@@ -0,0 +1,31 @@
+function [V_Put, V_Perf] = Pricings_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph, B_Y, B_Put, coeff_ph, coeff_y)
+    V_Put = zeros(length(S0_values), 1);
+    V_Perf = zeros(length(S0_values), 1);
+
+    for idx = 1:length(S0_values)
+        S0 = S0_values(idx);
+        K = S0;
+
+        % Coupons
+        C_Ph = coeff_ph * S0; % % of S0
+        C_Y = coeff_y * S0; % % of S0
+
+
+        % Initialize payoffs
+        Payoff_Put = zeros(N, 1);
+        Payoff_Perf = zeros(N, 1);
+
+        % Simulate trajectories and compute payoffs
+        for n = 1:N
+            S = Simulate_Underlying(S0, r, sigma, T, T / delta_t); % Single trajectory
+
+            % Compute Payoffs
+            Payoff_Put(n) = Compute_Put_Payoff(S, K, Pi0, C_Ph, C_Y, T, r, B_Ph, B_Y, B_Put, delta_t);
+            Payoff_Perf(n) = Compute_Perf_Payoff(S, Pi0, C_Ph, C_Y, T, r, B_Ph, B_Y, B_Put, delta_t);
+        end
+
+        % Average payoffs
+        V_Put(idx) = mean(Payoff_Put);
+        V_Perf(idx) = mean(Payoff_Perf);
+    end
+end
