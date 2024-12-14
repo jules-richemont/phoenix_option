128f3f9185cb04606d24b0c337da9bdd36964639
Ambroise THIBAULT <92878091+AmbroiseTHIBAULT@users.noreply.github.com>
2024-12-15 00:54:50 +0100

    taking in account proportinality

--- Pricing/Main_Pricing.m
+++ Pricing/Main_Pricing.m
@@ -6,24 +6,51 @@ T = 10;             % Maturity in years
 delta_t = 1;        % Observation frequency in years
 sigma = 0.3;        % Volatility
 r = 0.02;           % Risk-free rate
-N = 10000;         % Number of simulations
+N = 1000;         % Number of simulations
 timesteps = T / delta_t;
 S0_values = 80:1:120; % S0 varying from 80 to 120
 
+
+% ---------- Fixed barriers
 % Barriers
-B_Ph = 120;    % Fixed autocall barrier
-B_Y = 80;      % Fixed coupon barrier
-B_Put = 70;    % Fixed put barrier
+B_Ph_1 = 120;    % Fixed autocall barrier
+B_Y_1 = 80;      % Fixed coupon barrier
+B_Put_1 = 70;    % Fixed put barrier
+B_Ph_2 = 100;    % Fixed autocall barrier
+B_Y_2 = 70;      % Fixed coupon barrier
+B_Put_2 = 60;    % Fixed put barrier
 coeff_ph_1 = 0.1;
 coeff_y_1 = 0.05;
 coeff_ph_2 = 0.08;
 coeff_y_2 = 0.05;
-
+not_proportionnal = 0;
 % Pricing cases
-V_Case1_Put = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph, B_Y, B_Put, coeff_ph_1, coeff_y_1, 1);
-V_Case1_Perf = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph, B_Y, B_Put, coeff_ph_1, coeff_y_1, 0);
-V_Case2_Put = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph, B_Y, B_Put, coeff_ph_2, coeff_y_2, 1);
-V_Case2_Perf = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph, B_Y, B_Put, coeff_ph_2, coeff_y_2, 0);
+V_Case1_Put = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph_1, B_Y_1, B_Put_1, coeff_ph_1, coeff_y_1, 1, not_proportionnal);
+V_Case1_Perf = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph_1, B_Y_1, B_Put_1, coeff_ph_1, coeff_y_1, 0, not_proportionnal);
+V_Case2_Put = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph_2, B_Y_2, B_Put_2, coeff_ph_2, coeff_y_2, 1, not_proportionnal);
+V_Case2_Perf = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph_2, B_Y_2, B_Put_2, coeff_ph_2, coeff_y_2, 0, not_proportionnal);
+
+
+% ---------- Fixed barriers
+% Barrier
+B_Ph_coeff_1 = 1.2;   
+B_Y_coeff_1 = 0.8;     
+B_Put_coeff_1 = 0.7;  
+B_Ph_coeff_2 = 1.0;
+B_Y_coeff_2 = 0.7;
+B_Put_coeff_2 = 0.6;
+
+coeff_ph_1 = 0.1;
+coeff_y_1 = 0.05;
+coeff_ph_2 = 0.08;
+coeff_y_2 = 0.05;
+proportionnal = 1;
+% Pricing cases
+V_Case1_Put_Prop = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph_coeff_1, B_Y_coeff_1, B_Put_coeff_1, coeff_ph_1, coeff_y_1, 1, proportionnal);
+V_Case1_Perf_Prop = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph_coeff_1, B_Y_coeff_1, B_Put_coeff_1, coeff_ph_1, coeff_y_1, 0, proportionnal);
+V_Case2_Put_Prop = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph_coeff_2, B_Y_coeff_2, B_Put_coeff_2, coeff_ph_2, coeff_y_2, 1, proportionnal);
+V_Case2_Perf_Prop = Pricing_Phoenix(S0_values, N, T, r, sigma, delta_t, Pi0, B_Ph_coeff_2, B_Y_coeff_2, B_Put_coeff_2, coeff_ph_2, coeff_y_2, 0, proportionnal);
+
 
 
 % Plot results
